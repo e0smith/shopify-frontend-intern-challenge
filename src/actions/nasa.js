01547362474abd657ca672a7ccec7be644c2e7d2
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-09-18 18:14:00 -0400

    nasa.js fetch working.

---
+++ src/actions/nasa.js
@@ -0,0 +1,12 @@
+export const getData = () => {
+    return (dispatch) => {
+        fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2000&camera=fhaz&api_key=Uxko0rZrgWQg2VPgakQFEF4GRJXcBnxRVZx9Luch")
+            .then(resp => resp.json())
+            .then(photos => {
+                dispatch({
+                    type: "GET_NASA",
+                    payload: photos
+                })
+            })
+        }
+}
