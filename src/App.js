a0439f670d4efe1ff6a49be033b44b75ed71307f
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-09-18 18:11:24 -0400

    app.js layout. Incomplete. Sends store information to card.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { getData } from './actions/nasa'
+import PhotoCard from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  componentDidMount(){
+    this.props.getData()
+  }
+
+
+    state = {
+      like: 0
+    }
+
+  handleChange = (event) => {
+    this.setState({like: event.target.value})
+  }
+
+  render(){
+    return(
+      <div className="cards">
+        {this.props.photos.map((photo) => (<PhotoCard photo={photo} />))}
+      </div>
+       )
+  }
 }
-
-export default App;
+const mapStateToProps = (state) => {
+  return{
+    photos: state.photos,
+    loading: state.loading
+  }
+}
+export default connect(mapStateToProps, { getData })(App);
