923e01f0b05ac5c9baee173a1202354faf7819b2
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-09-19 17:14:02 -0400

    Home.js file created, App.js logic moved here

---
+++ src/components/pages/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PhotoCard from '../Card';
+
+class Home extends Component {
+    state = {
+        like: 1
+    }
+
+    handleChange = (event) => {
+        this.setState({like: event.target.value})
+    }
+    
+    render(){
+        return(
+            <div className="cards">
+                {this.props.photos.map(nasa => nasa.photos.map(data => <PhotoCard photo={data} likeInc={this.state.like}/>))}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return{
+      photos: state.photos
+    }
+}
+
+export default connect(mapStateToProps)(Home);
