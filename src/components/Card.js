e0c83a23bf273742a165cbe38f33b8ab4c51ea33
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-09-19 15:28:38 -0400

    card file cleaned up

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Component } from 'react';
+// import { Component } from 'react';
 
-
-class PhotoCard extends Component {
-
-    render(){
-        // const { img_src, earth_date } = photo
-        console.log("photo: ", this.props.photo.photos)
-        // console.log("image: ", img_src)
-        return(
-            <div class="card">
-                {/* <img src={img_src} class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h1>{earth_date}</h1>
-                </div> */}
-            </div>
-        )
-    }
+export const PhotoCard = (photo) => {
+    console.log(photo.photo)
+    return(
+        <div>    
+        </div>
+    )
 }
-
 
 export default PhotoCard;
