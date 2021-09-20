ac7bdf6959c213d01084c85ab900cfd39d82a252
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-09-20 16:35:41 -0400

    Home.js cleaned up

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PhotoCard from '../Card/Card'
+import PhotoCard from '../card/Card'
 
 class Home extends Component {
-    state = {
-        like: 1
-    }
-
-    handleChange = (event) => {
-        this.setState({like: event.target.value})
-    }
-    
     render(){
         return(
             <div className="cards">
-                {this.props.photos.map(nasa => nasa.photos.map(data => <PhotoCard photo={data} likeInc={this.state.like}/>))}
+                {this.props.photos.map(nasa => nasa.photos.map(data => <PhotoCard photo={data} />))}
             </div>
         )
     }
