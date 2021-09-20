ccd146a22a4ea55400bad89e7186f42e13727670
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-09-20 18:52:27 -0400

    fetch url temporarily changed

--- src/actions/nasa.js
+++ src/actions/nasa.js
@@ -3,7 +3,7 @@ import {sp, op, cu } from '../components/SettingSelect'
 
 export const getData = () => {
     return (dispatch) => {
-        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${cu}/photos?sol=2000&camera=fhaz&api_key=Uxko0rZrgWQg2VPgakQFEF4GRJXcBnxRVZx9Luch`)
+        fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2000&camera=fhaz&api_key=Uxko0rZrgWQg2VPgakQFEF4GRJXcBnxRVZx9Luch")
             .then(resp => resp.json())
             .then(photos => {
                 dispatch({
